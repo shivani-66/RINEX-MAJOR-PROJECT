bc4411722f6a87e8d649351ef450cf8c0c21b0c8
shivani-66 <100506872+shivani-66@users.noreply.github.com>
2022-03-05 14:16:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,10 @@ model.fit(iris.data,iris.target)
 op = model.predict([[sl,sw,pl,pw]])
 op = iris.target_names[op[0]]
 st.title(op)
-                                                       
+         
+      
+        
+          
 
 
                            
